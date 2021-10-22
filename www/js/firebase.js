47bf8c7756e557b216b404d2890f371d952e334a
unknown <luccasalzira@gmail.com>
2021-10-22 20:13:23 -0300

    Banco de Dados renovado

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-	apiKey: "AIzaSyBh8UvnDbj9uoph98xBQgLtp_ejviVumf0",
-    authDomain: "testeaula-417e3.firebaseapp.com",
-    projectId: "testeaula-417e3",
-    storageBucket: "testeaula-417e3.appspot.com",
-    messagingSenderId: "742476696416",
-    appId: "1:742476696416:web:240b4b89a11fe8e9ebec05",
-    measurementId: "G-6X3FPDP0K9"
+	apiKey: "AIzaSyALgf68A3iqs8XDT7LcinEGgyx7uHXA9AU",
+    authDomain: "testepab-2e22f.firebaseapp.com",
+    projectId: "testepab-2e22f",
+    storageBucket: "testepab-2e22f.appspot.com",
+    messagingSenderId: "603537404685",
+    appId: "1:603537404685:web:574ac331cca530f3717644",
+    measurementId: "G-38H03X2MG0"
  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
